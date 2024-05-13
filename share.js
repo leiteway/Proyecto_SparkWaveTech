37de74064925ab4b18902a9175f4ef4aba0cd96e
Leite <leite.way@gmail.com>
2024-05-13 11:14:16 +0200

    chore(WIP-share-on-linkedin

---
+++ share.js
@@ -0,0 +1,24 @@
+function Share(description) {
+    const url = window.location.href;
+
+    function ShareWebAPI() {
+        if (navigator.share) {
+            navigator.share({
+                title: description,
+                url: url,
+                images: [
+                    {
+                        url: 'https://example.com/your-image-url.jpg', // Asegúrate de reemplazar esto con la URL de tu imagen
+                        alt: description,
+                    },
+                ],
+            })
+              .catch(err => alert("Error Sharing: " + err))
+        }
+    }
+
+    ShareWebAPI();
+}
+
+// Llama a la función Share con el título que deseas compartir
+Share('Título del contenido que deseas compartir');
